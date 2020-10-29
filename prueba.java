d61326b9b00d40c66b9eaef0926e090fd0447d4e
luis <lmmartineg@eafit.edu.co>
2020-10-28 19:29:25 -0500

    Intercambio inicial de proyecto

---
+++ prueba.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class prueba here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class prueba
+{
+    public static void main(String [] args) {
+    
+    int[][]muro=new int[5][5];
+    muro[1][1]=5;
+    
+    int[][]pepe=new int[5][5];
+    pepe[2][2]=4;
+    if(pepe[2][2]==muro[1][1]){
+        
+        System.out.println("que");
+        
+    }
+    else{
+    System.out.println("F");
+    }
+    System.out.println(12 %10);
+    System.out.println(3 %10 );
+    System.out.println(10 %10 );
+    System.out.println(30 %10 );
+    }
+}
